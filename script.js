5c7d2cdf194de62ad2726aa9f4ab6f54c6a2a832
Sviatko Pylyp <pylyp.sviatoslav@gmail.com>
2020-09-15 10:33:32 +0300

    some changes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-<<<<<<< HEAD
 function PrintSum(a, b) {
     return a + b;
-=======
+}
 //create the function 
 function PrintMinus(a, b) {
     return a - b;
->>>>>>> create_a_minusFunction
 }
 
 let check = PrintSum(5, 3);
-
 console.log(check);
+
+let check1 = PrintMinus(5, 3);
+console.log(check1);
